a71b08c604977c992ade9ee7842cb0f18b88162e
damfinkel <damfinkel@gmail.com>
2020-04-03 19:02:32 -0300

    added route guards and removed some unused components

--- src/router.js
+++ src/router.js
@@ -6,20 +6,23 @@ import Home from './views/Home.vue'
 
 Vue.use(Router)
 
-export default new Router({
-  routes: [
-    {
-      path: '/signup',
-      component: Signin
-    },
-    {
-      path: '/login',
-      component: Login
-    },
-    {
-      path: '/home',
-      component: Home
-    }
-  ],
-  mode: 'history'
+const privateRoutes = [{ path: '/signup', component: Signin }, { path: '/login', component: Login }]
+const publicRoutes = [{ path: '/home', component: Home }]
+
+const routes = [
+  ...privateRoutes,
+  ...publicRoutes
+]
+
+const router = new Router({ routes: routes, mode: 'history' })
+
+const getIsAuthenticated = () => !!localStorage.accessToken
+
+router.beforeEach((to, from, next) => {
+  const isAuthenticated = getIsAuthenticated()
+  if (publicRoutes.find(route => route.path === to.path) && !isAuthenticated) next({ path: '/login' })
+  if (privateRoutes.find(route => route.path === to.path) && isAuthenticated) next({ path: '/home' })
+  else next()
 })
+
+export default router
